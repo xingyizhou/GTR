459be7c27af75e745ec2bfe98facc8d81b7c7fcf
noahcao <jinkuncao@gmail.com>
2022-04-13 16:55:53 -0400

    fix typo to keep not just "-FRCNN" seqs

--- tools/convert_mot2coco.py
+++ tools/convert_mot2coco.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     for seq in sorted(seqs):
       if '.DS_Store' in seq:
         continue
-      if 'mot17' in DATA_PATH and (split != 'test' and not ('FRCNN' in seq)):
+      if 'MOT17' in DATA_PATH and (split != 'test' and not ('FRCNN' in seq)):
         continue
       video_cnt += 1
       out['videos'].append({
